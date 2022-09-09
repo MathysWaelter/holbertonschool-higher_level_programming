8c7aa8b89f40b8f4ce09b382989059af05d79751
root <mathys.laurent3164@gmail.com>
2022-09-09 01:38:47 -0700

    add add function for argv[i]

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+	sum = 0
+	for i in range (1, len(argv)):
+		if (len(argv) - 1) == 0:
+			print('0')
+		else:
+			sum += int(argv[i])
+print(sum)
