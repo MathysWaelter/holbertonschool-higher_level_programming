eff3d7d6f8edaf862324443e6c85b82d726faed5
root <mathys.laurent3164@gmail.com>
2022-09-09 00:43:14 -0700

    add function to count argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-if (len(argv) - 1) == 0:
-    print('0 arguments.')
-else:
-    print("{} {}:".format(len(argv) - 1,
-          "argument" if len(argv) - 1 == 1 else "arguments"))
-for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    if (len(argv) - 1) == 0:
+        print('0 arguments.')
+    else:
+        print("{} {}:".format(len(argv) - 1,
+            "argument" if len(argv) - 1 == 1 else "arguments"))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
