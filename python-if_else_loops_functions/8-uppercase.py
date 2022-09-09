6c5a09db544b17ab7c1eac72692f11c05286aa68
root <mathys.laurent3164@gmail.com>
2022-09-09 02:19:36 -0700

    add step 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,9 @@ def islower(c):
             return 1
     return 0
 
+
 def uppercase(str):
     for i in str:
-        print("{}".format(chr(ord(i) \
-        if not islower(i) else ord(i) - 32)), end='')
+        print("{}".format\
+        (chr(ord(i)if not islower(i) else ord(i) - 32)), end='')
     print('')
