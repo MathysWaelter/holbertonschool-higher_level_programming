b38398b5571bda737e40c416fc4016312c458d43
root <mathys.laurent3164@gmail.com>
2022-09-13 03:27:21 -0700

    add function to multiple return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	size = len(sentence)
-	if size == 0:
-		tmp = None
-	else:
-		tmp = sentence[0]
-	return ((size, tmp))
+    size = len(sentence)
+    if size == 0:
+        tmp = None
+    else:
+        tmp = sentence[0]
+    return ((size, tmp))
