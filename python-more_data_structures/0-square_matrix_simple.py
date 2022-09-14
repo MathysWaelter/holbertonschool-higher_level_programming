fa5e36bab699301ac58165fbb07eefd1da5b67fb
root <mathys.laurent3164@gmail.com>
2022-09-14 12:25:35 -0700

    add function to print power matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_tmp = []
+    if len(matrix) > 0:
+        for i in matrix[:]:
+            matrix_tmp.append(list(map(lambda x: x ** 2, i)))
+    return matrix_tmp
