e78641008dd63de2561ef090c37e0998c358761a
root <mathys.laurent3164@gmail.com>
2022-09-16 13:04:18 -0700

    add function to safe print

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         sum = a / b
     except ZeroDivisionError:
-        result = None
+        sum = None
     finally:
         print("Inside result: {}".format(sum))
         return sum
