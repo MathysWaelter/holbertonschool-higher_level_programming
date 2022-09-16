b0055d4a17c2433bb50fea1dfcc097cc2daecbba
root <mathys.laurent3164@gmail.com>
2022-09-16 02:26:17 -0700

    add function to print int

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if value or value == 0:
-            print("{:d}".format(value), end = '\n')
+            print("{:d}".format(value), end='\n')
             return True
         else:
             return False
