78470f6ded02bc8dcb2b8c963fa231eddc52df23
root <mathys.laurent3164@gmail.com>
2022-09-19 03:15:02 -0700

    add function to raise error for square

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    function to create square
+"""
+
+
+class Square():
+    """
+    Square:
+        it's square
+        :param value:
+        value of size
+    """
+
+    def __init__(self, __size=0, message=""):
+        """
+        :param value:
+            value of size and message for raise
+        """
+
+        if not isinstance(__size, int):
+            raise TypeError("size must be an integer".format(message))
+        if __size < 0:
+            raise NameError("size must be >= 0".format(message))
+        self.__size = __size
