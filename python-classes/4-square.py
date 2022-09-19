f4dea548eb22ca5821ba0382e19304dca66da603
root <mathys.laurent3164@gmail.com>
2022-09-19 13:31:33 -0700

    update size and value

---
+++ python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+    function to create square
+"""
+
+
+class Square():
+    """
+    Square:
+        it's square
+        :param value:
+        value of size
+    """
+
+    def __init__(self, size=0, message=""):
+        """
+        :param value:
+            value of size and message for raise
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        :param value:
+            value of size for return current area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        :param value:
+            value for define a square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise NameError("size must be >= 0")
+        self.__size = value
