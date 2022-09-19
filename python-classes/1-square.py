20966c8cfdc484155e2c13d1d78283a5817001fa
root <mathys.laurent3164@gmail.com>
2022-09-19 02:35:47 -0700

    function to define size of a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    function to create square
+"""
+
+
+class Square:
+    """
+    Square:
+        it's square
+        :param value:
+        value of size
+    """
+
+    def __init__(self, __size):
+        self.__size = __size
