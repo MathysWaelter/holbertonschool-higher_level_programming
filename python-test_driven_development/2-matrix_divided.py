7d02cde0a6f28ea6e7900d581031ff51bcb79d8f
root <mathys.laurent3164@gmail.com>
2022-09-20 06:57:57 -0700

    add function to print matrix divide

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,12 +3,12 @@
 def custom_divid(a):
   return float("%0.2f" % a) 
 def matrix_divided(matrix, div):
-  #  if type(matrix) not in [int, float] or matrix != matrix:
- #       raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-  #  if type(div) not in [int, float] or div != div:
- #       raise TypeError("div must be a number")
-#    if div == 0:
- #       raise ZeroDivisionError("division by zero")
+    if (all([isinstance(item, int) for item in matrix])):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) not in [int, float] or div != div:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     
     return [[custom_divid(item / div) for item in liste] for liste in matrix]
     
