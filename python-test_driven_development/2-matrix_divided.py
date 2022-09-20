cb3d49ae9e71d0fe7524047e0b4be60ba6d7e51c
root <mathys.laurent3164@gmail.com>
2022-09-20 11:41:42 -0700

    fix step 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,8 +12,18 @@ def custom_divid(a):
 
 def matrix_divided(matrix, div):
     """divide matrix"""
+    size_0 = len(matrix[0])
+    b = 0
+    i = 0
+    for a in matrix:
+        if len(a) == size_0:
+            b = + 1
+        i += 1
+    if b != i:
+        raise TypeError("Each row of the matrix must have the same size")
     if (all([isinstance(item, int) for item in matrix])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if type(div) not in [int, float] or div != div:
         raise TypeError("div must be a number")
     if div == 0:
