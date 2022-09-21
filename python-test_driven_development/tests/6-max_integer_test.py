070af716c8229a8ecd26a79f80f333da7dbe8d45
root <mathys.laurent3164@gmail.com>
2022-09-21 14:55:00 -0700

    add unittest for maxinteger func

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 
 
@@ -9,8 +10,18 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def basic_test(self):
         self.assertEqual(max_integer([2, 9, 1]), 9)
+
+    def negative_test(self):
         self.assertEqual(max_integer([-5, -9, -11]), -5)
+
+    def empty_test(self):
         self.assertEqual(max_integer([]), None)
+
+    def basic_negative_test(self):
         self.assertEqual(max_integer([2, 3, 5, -11, 666]), 666)
+
+    def float_test(self):
         self.assertEqual(max_integer([4, 7, 3, 7.8]), 7.8)
+
+    def single_test(self):
         self.assertEqual(max_integer([8]), 8)
