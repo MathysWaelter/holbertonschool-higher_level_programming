9669fb2498c313b214c59196b5f2b189787f796c
root <mathys.laurent3164@gmail.com>
2022-09-21 06:52:42 -0700

    add functio nto indent

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function to to check test indent"""
+
+
+def text_indentation(text):
+    """checker"""
+    if type(text) is not str or text != text:
+        raise TypeError("text must be a string")
+    if text:
+        text = text.replace('.', '.\n\n')
+        text = text.replace('!', '!\n\n')
+        text = text.replace(':', ':\n\n')
+    print(text)
+    
