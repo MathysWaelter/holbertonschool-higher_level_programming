2185366b27c230e75749e1ae981291654eef1915
root <mathys.laurent3164@gmail.com>
2022-09-21 06:06:42 -0700

    add function to print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    function to create square
+"""
+from ast import Raise
+
+
+def print_square(size):
+    """
+    :param value:
+        print square with #
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size > 0:
+        for x in range(size):
+            print()
+            for space in range(size):
+                 print("#", end="")
+    elif size < 0:
+        print()
+        raise ValueError("size must be >= 0")
