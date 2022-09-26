198c6c95dd0615534a59797211bf15d79cd4a74a
root <mathys.laurent3164@gmail.com>
2022-09-26 06:38:03 -0700

    add function to sort a list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""func for inherits another class"""
+
+
+class list:
+    """list of int"""
+
+    def print_sorted(self):
+        """for sort"""
+        return (sorted(self))
