28634a662e2bfbaf3d91cf2c4930a3bf4793092c
root <mathys.laurent3164@gmail.com>
2022-09-27 03:32:22 -0700

    add function to return a area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a geometry print"""
+
+
+class BaseGeometry:
+    """Class for print a bass geometry"""
+
+    def area(self):
+        """method for return area"""
+        raise Exception("area() is not implemented")
