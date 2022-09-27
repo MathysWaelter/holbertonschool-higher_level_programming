7b10e915190bf4f68bd08c7c095eb1d81d6ee44a
root <mathys.laurent~@gmail.com>
2022-09-27 04:20:06 -0700

    add new instance methods

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""define a geometry print"""
+
+
+class BaseGeometry:
+    """Class for print a bass geometry"""
+
+    def __init__(self, name='', value=0):
+        self.name = name
+        self.value = value
+
+    def area(self):
+        """method for return area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method for check type of variable"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
