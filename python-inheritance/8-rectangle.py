fcc1a26bf98a5472c3ce6b0f914ff142ce1dd1d3
root <mathys.laurent~@gmail.com>
2022-09-27 05:33:33 -0700

    add a inheritance function

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """define a geometry print"""
 
 
-class BaseGeometry:
-    """Class for print a bass geometry"""
-
-    def area(self):
-        """method for return area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method for check type of variable"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Class for print a rectangle"""
