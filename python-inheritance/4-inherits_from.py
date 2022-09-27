0903a9107bd77cf901dfaf401c306971f15a2de6
root <mathys.laurent3164@gmail.com>
2022-09-27 03:25:56 -0700

    add function to check id a instance is inheritance

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an inheritable"""
+
+
+def inherits_from(obj, a_class):
+    """define true of false"""
+    if type(obj) == a_class:
+        return False
+    else:
+        return True
