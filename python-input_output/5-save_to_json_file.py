9a167af9d7113ca1d6514f057c10b49ff7498efa
root <mathys.laurent3164@gmail.com>
2022-09-28 05:42:25 -0700

    fix step 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """write and return"""
     with open(filename, 'w', encoding='utf-8') as JsonFile:
-        return json.dump(JsonFile)
+        return json.dump(filename, JsonFile)
