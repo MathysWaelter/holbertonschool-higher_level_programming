a4ef9e13fdb2f1b2884f65d08df84c8baba1e3b7
root <mathys.laurent3164@gmail.com>
2022-09-28 02:22:36 -0700

    add function to append in file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""function to open and write in file"""
+"""function to open and append in file"""
 
 
-def write_file(filename="", text=""):
-    """open and write"""
+def append_file(filename="", text=""):
+    """open and append"""
+
     with open(filename, mode='a', encoding='utf-8') as file:
         file.write(text)
         return len(text)
