ab18b8fea227e72919bc2d8c5a33d3e9da7f744c
root <mathys.laurent3164@gmail.com>
2022-09-28 02:02:06 -0700

    add function to read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""function to open and read file"""
+
+
 def read_file(filename=""):
+    """"open and read"""
     with open(filename, encoding='utf-8') as file:
         read_file = file.read()
