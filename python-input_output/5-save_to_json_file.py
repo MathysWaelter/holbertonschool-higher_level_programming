7bacffac8894867dd8be66e35694363b9d59775f
root <mathys.laurent3164@gmail.com>
2022-09-28 05:16:32 -0700

    add function to write an object in JSON file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" function that writes an Object to a text file, using a JSON representation"""
+""" function that writes an Object to a text file using JSON representation"""
 
 
 import json
@@ -8,5 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """write and return"""
     JsonStr = json.dumps(my_obj)
-    with open (filename, 'w', encoding='utf-8') as JsonFile:
+    with open(filename, 'w', encoding='utf-8') as JsonFile:
         JsonFile.write(JsonStr)
