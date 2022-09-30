32ec854f036e34bd70b9bea52f2b0b2cd6cb79b0
root <mathys.laurent3164@gmail.com>
2022-09-30 07:07:31 -0700

    add function to save a json file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,11 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == '__main__':
-    try:
-        jsonfile = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        jsonfile = []
+try:
+    jsonfile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    jsonfile = []
 
-    for i in range(1, len(sys.argv)):
-        jsonfile.append(sys.argv[i])
-    save_to_json_file(jsonfile, "add_item.json")
+for i in range(1, len(sys.argv)):
+    jsonfile.append(sys.argv[i])
+save_to_json_file(jsonfile, "add_item.json")
