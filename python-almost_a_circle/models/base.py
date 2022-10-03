befa2f9fd4322abab344a7a9fb2f853a00f778eb
root <you@example.com>
2022-10-03 01:37:17 -0700

    fix int problem

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,11 +4,10 @@
 
 class Base:
     """main class"""
-
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None and type(id) is int:
+        if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
