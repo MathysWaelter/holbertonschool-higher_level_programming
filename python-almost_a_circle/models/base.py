0dd80bfaa8375d44176bebd486260ea069c0f28e
root <you@example.com>
2022-10-03 01:46:35 -0700

    fix init

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None and type(id) is not int:
+        if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
