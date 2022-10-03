61a407f24835f1e4da0ec82bdb1fd01dbe6018ab
root <you@example.com>
2022-10-03 03:09:55 -0700

    add displya function for print size detail of a rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -54,8 +54,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """function for display size of rectangle"""
-        return("[Rectangle] ({}) {}/{} - {}/{}".format\
-            (self.id, self.__x, self.__y, self.__width, self.__height))
+        return("[Rectangle] ({}) {}/{} - {}/{}".format
+        (self.id, self.__x, self.__y, self.__width, self.__height))
 
     @property
     def width(self):
