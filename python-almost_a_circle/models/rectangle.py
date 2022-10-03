d0c07edb8fad9c43deb8cef66c2b75a392a71d13
root <you@example.com>
2022-10-03 02:49:16 -0700

    add raise for all variable

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,9 +75,9 @@ class Rectangle(Base):
             value for x of a rectangle
         """
         if type(value) != int:
-            raise TypeError("height must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -95,7 +95,7 @@ class Rectangle(Base):
             value for x of a rectangle
         """
         if type(value) != int:
-            raise TypeError("height must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
