f7f87c86a56648994e82b1b78f0ae71513d1a297
root <you@example.com>
2022-10-04 05:12:46 -0700

    fix init problem

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,12 +2,12 @@
 """new class Square inherit from Base.Rectangle"""
 
 
-from models.base import Base
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Square Class"""
+    
     def __init__(self, size, x=0, y=0, id=None):
         """retrieve __init__ from Rectangle Class"""
         super().__init__(size, size, x, y, id)
