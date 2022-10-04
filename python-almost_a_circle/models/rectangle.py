828a9e2230eb22ad96a86176de3fd26904361573
root <you@example.com>
2022-10-04 06:07:14 -0700

    add function to print dict

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     """Class inherits from SuperClass"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Class inherits from SuperClass"""
         super().__init__(id)
         self.width = width
         self.height = height
