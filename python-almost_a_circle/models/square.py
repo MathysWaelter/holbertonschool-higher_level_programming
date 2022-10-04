878cb2eea27a8e5e85968a6a4bb210960c00ad69
root <you@example.com>
2022-10-04 04:58:39 -0700

    add new class Square inherit from Rectangle class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
     """Square Class"""
     def __init__(self, size, x=0, y=0, id=None):
         """retrieve __init__ from Rectangle Class"""
-        super(Square, self).__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
