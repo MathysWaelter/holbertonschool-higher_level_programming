f8ba018c21288b11576273870a24bcb32252474d
root <you@example.com>
2022-10-05 06:19:46 -0700

    add function to return a dict

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return Json string of a list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
