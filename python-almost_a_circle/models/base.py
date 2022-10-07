8d4eadc79b00e8e496647d7d1f18ad624f873d8c
root <you@example.com>
2022-10-07 10:09:05 -0700

    fix tojsonstring return*

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return Json string of a list_dictionaries"""
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
