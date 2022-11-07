3e764fcce9066cb32b5c579f4f603748b41c75dc
root <you@example.com>
2022-11-07 03:02:46 -0800

    create database and table with foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    cities_id INT FOREIGN KEY (id) cities(cities_id),
+    state_id INT FOREIGN KEY (id) cities(cities_id),
     name VARCHAR(256) NOT NULL
 );
