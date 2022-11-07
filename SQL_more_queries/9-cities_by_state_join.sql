7694870dce6eedc07cf31721bea548ca71d1d118
root <you@example.com>
2022-11-07 05:07:50 -0800

    lists all the cities that can be found in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all the cities that can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities
-JOIN states on cities.state_id
-ORDER BY state_id;
+JOIN states on cities.state_id=states.id
+ORDER BY cities.id;
