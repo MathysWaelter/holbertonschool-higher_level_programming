0498fc8ffdd03b00cecc305337266dba2d109003
root <you@example.com>
2022-11-08 06:32:21 -0800

    lists all states from the database hbtn_0e_0_usa*

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+"""Write a script that lists all states from the database hbtn_0e_0_usa:
+        -Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+        -You must use the module MySQLdb (import MySQLdb)
+        -Your script should connect to a MySQL server running on localhost at port 3306
+        -Results must be sorted in ascending order by states.id
+        -Results must be displayed as they are in the example below
+        -Your code should not be executed when imported"""
 
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
+    # DECLARE MY ARGV FOR EXECUTE IN TERMINAL
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
+    # CONNECT TO MY EXTERN DATABASE 
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
+    # ITS A ABSTRACTION
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    # FOR REMAIMING TUPLE    
+    result = cur.fetchall()
+    for x in result:
+        print(x )
 
-db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
+    # TEST WITH FETCHONE
+    # print ("(%s, '%s')" % cur.fetchone())
+    # RESULT :
+    # (1, 'California')
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-result = cur.fetchall()
-for x in result:
-    print(x )
-
-cur.close()
-
-db.close()
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
