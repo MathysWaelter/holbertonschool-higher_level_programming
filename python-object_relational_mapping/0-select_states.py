48e8f26340100a98c79f1f892eb41bcb2fe38db7
root <you@example.com>
2022-11-08 06:34:16 -0800

    lists all states from the database hbtn_0e_0_usa*

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Write a script that lists all states from the database hbtn_0e_0_usa:
-        -Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+        -take 3 arguments: mysql username, mysql password and database name
         -You must use the module MySQLdb (import MySQLdb)
-        -Your script should connect to a MySQL server running on localhost at port 3306
         -Results must be sorted in ascending order by states.id
         -Results must be displayed as they are in the example below
         -Your code should not be executed when imported"""
@@ -18,14 +17,15 @@ if __name__ == '__main__':
     database_name = sys.argv[3]
 
     # CONNECT TO MY EXTERN DATABASE 
-    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host='localhost', user=username, 
+                          passwd=password, db=database_name)
     # ITS A ABSTRACTION
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    # FOR REMAIMING TUPLE    
+    # FOR REMAIMING TUPLE
     result = cur.fetchall()
     for x in result:
-        print(x )
+        print(x)
 
     # TEST WITH FETCHONE
     # print ("(%s, '%s')" % cur.fetchone())
