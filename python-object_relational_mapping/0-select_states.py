6a21ee8125efce12403653718235eb188f805284
root <you@example.com>
2022-11-08 02:46:49 -0800

    lists all states from the database hbtn_0e_0_usa*

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,27 +11,27 @@
 import MySQLdb
 import sys
 
-# DECLARE MY ARGV FOR EXECUTE IN TERMINAL
+"""DECLARE MY ARGV FOR EXECUTE IN TERMINAL"""
 username = sys.argv[1]
 password = sys.argv[2]
 database_name = sys.argv[3]
 
-# CONNECT TO MY EXTERN DATABASE 
+"""CONNECT TO MY EXTERN DATABASE"""
 db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
-# ITS A ABSTRACTION
+"""ITS A ABSTRACTION"""
 cur = db.cursor()
 cur.execute("SELECT * FROM states")
-# FOR REMAIMING TUPLE    
+"""FOR REMAIMING TUPLE"""
 result = cur.fetchall()
 for x in result:
     print(x )
 
-# TEST WITH FETCHONE
-# print ("(%s, '%s')" % cur.fetchone())
-# RESULT :
-# (1, 'California')
+"""TEST WITH FETCHONE
+    print ("(%s, '%s')" % cur.fetchone())
+    RESULT :
+    (1, 'California')"""
 
-# Close all cursors
+"""Close all cursors"""
 cur.close()
-# Close all databases
+"""Close all databases"""
 db.close()
