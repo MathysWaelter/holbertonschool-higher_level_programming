6fd3430934fb8ec8118105fbcaedb4924bf1e226
root <you@example.com>
2022-11-12 07:21:30 -0800

    fix problem from create engine and session

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,20 +2,22 @@
 """print the first state object"""
 
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    session = sessionmaker(bind=engine)()
-    result = session.query(State).first()
-    if result:
-        print(result)
-    else:
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1],
+                                    sys.argv[2],
+                                    sys.argv[3]),
+                            pool_pre_ping=True)
+    session = Session(engine)
+    re = session.query(State).order_by(State.id).first()
+    if re is None:
         print("Nothing")
-
+    else:
+        print("{}: {}".format(re.id, re.name))
     session.close()
