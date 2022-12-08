259b2c1908122dcdaa040833c703365080fcb728
root <you@example.com>
2022-12-08 13:39:55 -0800

    script for print multiple str

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,8 @@
 script that prints two arguments passed to it
 */
 
-multi_str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < multi_str.length; i++) {
-  console.log(multi_str[i]);
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
