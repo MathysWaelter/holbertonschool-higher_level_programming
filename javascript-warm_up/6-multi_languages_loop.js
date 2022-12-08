d3e06f88e2faeddbb328778148b52f92db7f5255
root <you@example.com>
2022-12-08 13:21:53 -0800

    script for print array with loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,8 @@
 script that prints two arguments passed to it
 */
 
-multi_str = ["C is fun", "Python is cool", "JavaScript is amazing"]
+multi_str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < multi_str.length; i++) {
-    console.log(multi_str[i]);
-  }
+  console.log(multi_str[i]);
+}
