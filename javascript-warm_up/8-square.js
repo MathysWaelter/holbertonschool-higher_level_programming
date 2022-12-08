254c40d3ccb19b4a76062e7948dd4a63f55c6dfe
root <you@example.com>
2022-12-08 13:57:39 -0800

    print square with argv

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+print square
+*/
+
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
