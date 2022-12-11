f749ff46c91ccd23e3a6474aac8dc3b4cdcab5a2
root <you@example.com>
2022-12-11 11:06:57 -0800

    new class square , super from rectangle

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/*
+create a rectangle class
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+/*
+create a square class hinerite from rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+ }
+}
+class Square extends Rectangle {
+    constructor (hw) {
+        super(hw, hw);
+    }
+ }
+
+
+module.exports = Rectangle;
+module.exports = Square;
