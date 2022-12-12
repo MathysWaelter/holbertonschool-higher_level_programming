99699f17e57f8004d0a78a3fad80f04c753f32b1
root <you@example.com>
2022-12-12 06:05:56 -0800

    scrap status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const imput = process.argv;
+
+request(imput[2], function (doesntexist, status) {
+  if (doesntexist) throw doesntexist;
+  console.log(status.statusCode);
+});
