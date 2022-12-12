cc810cdbe91c790944edfb3937a136a4e95280c0
root <you@example.com>
2022-12-12 05:42:14 -0800

    script for read a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const imput = process.argv;
+
+fs.readFile(imput[2], 'utf-8', function (doesntexist, txt) {
+    if (doesntexist) {
+        return console.log(doesntexist)
+    }
+    console.log(txt);
+});
