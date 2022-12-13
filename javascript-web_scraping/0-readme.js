e1ed4521cbca19db250f0696c2b1a225020c8076
root <you@example.com>
2022-12-13 04:15:43 -0800

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const imput = process.argv;
 
 fs.readFile(imput[2], 'utf-8', function (doesntexist, txt) {
-    if (doesntexist) {
-        return console.log(doesntexist)
-    }
-    console.log(txt);
+  if (doesntexist) {
+    return console.log(doesntexist);
+  }
+  console.log(txt);
 });
